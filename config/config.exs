8eae413403983c5e25755af5cd2a3e388c8183ac
Dmitry Ledentsov <dmlled@yahoo.com>
2025-03-15 23:03:21 +0100

    mix compile works

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@ config :pythonx, :uv_init,
   [project]
   name = "tbp_monty_via_pythonx"
   version = "0.0.0"
-  requires-python = "==3.8.*"
+  requires-python = "==3.10.*"
   dependencies = [
     "tbp.monty @ git+https://github.com/thousandbrainsproject/tbp.monty.git"
   ]
